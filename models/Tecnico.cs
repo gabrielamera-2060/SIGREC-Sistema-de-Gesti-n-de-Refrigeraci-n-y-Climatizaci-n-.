3608f5035648a262e8e32ba972d2c698e90632ae
gabriela.mera@tecnologicoliceocristiano.edu.ec <gabriela.mera@tecnologicoliceocristiano.edu.ec>
2026-07-20 15:59:57 -0500

    Agregar archivos de proyecto.

---
+++ models/Tecnico.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SIGREC__Sistema_de_Gestión_de_Refrigeración_y_Climatización__.models
+{
+    internal class Tecnico
+    {
+    }
+}
